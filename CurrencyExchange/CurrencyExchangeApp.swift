43225081d05dabb8ff7a18bea90241e351414516
Andrei Simvolokov <ssimboss@gmail.com>
2025-11-11 12:12:04 +0000

    Initial commit. Flags resources added to the project.

---
+++ CurrencyExchange/CurrencyExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangeApp.swift
+//  CurrencyExchange
+//
+//  Created by Andrei Simvolokov on 06/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
